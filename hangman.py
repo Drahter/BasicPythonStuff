496872f7e577c5151369bcb6d4f637c9dd968c52
Drahter <102571607+Drahter@users.noreply.github.com>
2022-08-19 13:41:27 +0500

    lives done

--- hangman.py
+++ hangman.py
@@ -3,12 +3,14 @@ import string
 
 from words import words
 
+
 def get_valid_word(words):
     word = random.choice(words)
     while '-' in word or ' ' in word:
         word = random.choice(words)
 
-    return word
+    return word.upper()
+
 
 def hangman():
     word = get_valid_word(words)
@@ -16,9 +18,12 @@ def hangman():
     alphabet = set(string.ascii_uppercase)
     used_letters = set()
 
+    lives = 7
 
-    while len(word_letters) > 0:
+    while len(word_letters) > 0 and lives >0:
+        print(f'You have {lives} lives, think better!')
         print('You have used these letters: ', ' '.join(used_letters))
+
         word_list = [letter if letter in used_letters else '-' for letter in word]
         print('Current word: ', ' '.join(word_list))
 
@@ -27,14 +32,14 @@ def hangman():
             used_letters.add(user_letter)
             if user_letter in word_letters:
                 word_letters.remove(user_letter)
+            else:
+                lives -= 1
 
         elif user_letter in used_letters:
             print('ALREADY USED! Try another letter')
 
         else:
             print('invalid character you dumb!')
-
-
 
 
 if __name__ == '__main__':
