efff389b466d2086c9d78a54f41bff14877b47d2
Drahter <102571607+Drahter@users.noreply.github.com>
2022-08-19 13:28:23 +0500

    graphics done

--- hangman.py
+++ hangman.py
@@ -16,17 +16,23 @@ def hangman():
     alphabet = set(string.ascii_uppercase)
     used_letters = set()
 
-    user_letter = input('Guess a letter: ').upper()
-    if user_letter in alphabet - used_letters:
-        used_letters.add(user_letter)
-        if user_letter in word_letters:
-            word_letters.remove(user_letter)
 
-    elif user_letter in used_letters:
-        print('ALREADY USED! Try another letter')
+    while len(word_letters) > 0:
+        print('You have used these letters: ', ' '.join(used_letters))
+        word_list = [letter if letter in used_letters else '-' for letter in word]
+        print('Current word: ', ' '.join(word_list))
 
-    else:
-        print('invalid character you dumb!')
+        user_letter = input('Guess a letter: ').upper()
+        if user_letter in alphabet - used_letters:
+            used_letters.add(user_letter)
+            if user_letter in word_letters:
+                word_letters.remove(user_letter)
+
+        elif user_letter in used_letters:
+            print('ALREADY USED! Try another letter')
+
+        else:
+            print('invalid character you dumb!')
 
 
 
