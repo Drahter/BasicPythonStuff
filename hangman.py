2d7a78b09d349298cd46aff62f6b4768accc2d11
Drahter <102571607+Drahter@users.noreply.github.com>
2022-08-19 13:23:06 +0500

    user input done

--- hangman.py
+++ hangman.py
@@ -1,4 +1,6 @@
 import random
+import string
+
 from words import words
 
 def get_valid_word(words):
@@ -10,3 +12,24 @@ def get_valid_word(words):
 
 def hangman():
     word = get_valid_word(words)
+    word_letters = set(word)
+    alphabet = set(string.ascii_uppercase)
+    used_letters = set()
+
+    user_letter = input('Guess a letter: ').upper()
+    if user_letter in alphabet - used_letters:
+        used_letters.add(user_letter)
+        if user_letter in word_letters:
+            word_letters.remove(user_letter)
+
+    elif user_letter in used_letters:
+        print('ALREADY USED! Try another letter')
+
+    else:
+        print('invalid character you dumb!')
+
+
+
+
+if __name__ == '__main__':
+    hangman()
